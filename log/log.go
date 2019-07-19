86682bc58935fdd08af9be291b188ea28261b1f5
Eyal Posener <posener@gmail.com>
2019-07-19 14:38:53 +0300

    fix log output

--- log/log.go
+++ log/log.go
@@ -10,7 +10,7 @@ type Logger interface {
 	Printf(format string, v ...interface{})
 }
 
-var Log Logger = stdlog.New(os.Stdout, "[gitfs] ", stdlog.LstdFlags)
+var Log Logger = stdlog.New(os.Stderr, "[gitfs] ", stdlog.LstdFlags)
 
 func Printf(format string, v ...interface{}) {
 	if Log == nil {
