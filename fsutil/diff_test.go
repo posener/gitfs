abc7a47a7a27f132234259d873795b44136ff64c
Eyal Posener <posener@gmail.com>
2019-08-02 10:03:56 +0300

    Improve diff

--- fsutil/diff_test.go
+++ fsutil/diff_test.go
@@ -13,27 +13,31 @@ func TestDiff(t *testing.T) {
 
 	a := make(tree.Tree)
 	a.AddFileContent("content-diff", []byte("1\n2\n"))
+	a.AddFileContent("content-equal", []byte("1\n2\n"))
 	a.AddFileContent("common-dir/only-in-a", []byte(""))
 	a.AddFileContent("file-in-a-dir-in-b", []byte(""))
 	a.AddDir("dir-in-a-file-in-b")
 
 	b := make(tree.Tree)
 	b.AddFileContent("content-diff", []byte("1\n3\n"))
+	b.AddFileContent("content-equal", []byte("1\n2\n"))
 	b.AddFileContent("common-dir/only-in-b", []byte(""))
 	b.AddDir("file-in-a-dir-in-b")
 	b.AddFileContent("dir-in-a-file-in-b", []byte(""))
 
-	want := FileSystemDiff{
-		{Path: "common-dir/only-in-a", Diff: msgOnlyInA},
-		{Path: "common-dir/only-in-b", Diff: msgOnlyInB},
-		{Path: "file-in-a-dir-in-b", Diff: msgAFileBDir},
-		{Path: "dir-in-a-file-in-b", Diff: msgADirBFile},
-		{Path: "content-diff", Diff: "content diff:\n 1\n-2\n+3"},
-	}
-
+	want := `Diff between a and b:
+[common-dir/only-in-a]: only in a
+[common-dir/only-in-b]: only in b
+[content-diff]: content diff (-a, +b):
+ 1
+-2
++3
+[dir-in-a-file-in-b]: on a is directory, on b file
+[file-in-a-dir-in-b]: on a is file, on b directory
+`
 	got, err := Diff(a, b)
 	require.NoError(t, err)
-	assert.ElementsMatch(t, want, got)
+	assert.Equal(t, want, got.String())
 }
 
 func TestDiffEmpty(t *testing.T) {
@@ -46,10 +50,10 @@ func TestDiffEmpty(t *testing.T) {
 
 	got, err := Diff(a, b)
 	require.NoError(t, err)
-	assert.ElementsMatch(t, FileSystemDiff{{Path: "foo", Diff: msgOnlyInA}}, got)
+	assert.ElementsMatch(t, []PathDiff{{Path: "foo", Diff: msgOnlyInA}}, got.Diffs)
 
 	// Mirror test
 	got, err = Diff(b, a)
 	require.NoError(t, err)
-	assert.ElementsMatch(t, FileSystemDiff{{Path: "foo", Diff: msgOnlyInB}}, got)
+	assert.ElementsMatch(t, []PathDiff{{Path: "foo", Diff: msgOnlyInB}}, got.Diffs)
 }
